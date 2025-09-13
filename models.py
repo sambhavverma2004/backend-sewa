9ac7286f44a509b79954833247c87377b604f105
Sambhav verma <vermasambhav8@gmail.com>
2025-09-13 12:39:20 +0530

    feat: Add phone number to User API response

--- models.py
+++ models.py
@@ -46,6 +46,7 @@ class Listing(db.Model):
             "title": self.title,
             "description": self.description,
             "price": self.price,
+            "phone_number": self.phone_number,
             "price_unit": self.price_unit,
             "includes_operator": self.includes_operator,
             "location_text": self.location_text,
